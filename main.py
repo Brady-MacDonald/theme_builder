1012c70bae17fac20a1711ef514d031fb4a1748f
Brady MacDonald <brady.macdonald@dal.ca>
2025-11-27 17:55:15 -0500

    Work on theme building from image

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+from typing import Union
 import sys
 from pathlib import Path
 
 from colorthief import ColorThief
 from jinja2 import Environment, FileSystemLoader
 
-output_location = "output"
+PathLike = Union[str, Path]
+
 if len(sys.argv) < 2:
     print("Usage: main.py <image>")
     exit(2)
@@ -17,21 +20,34 @@ def GetColors(image: str):
     return palette, dominant_color
 
 
-def Tester():
-    x = Environment(loader=FileSystemLoader("templates"))
-    template = x.get_template("tester.j2")
-    out = template.render({"test": "brady"})
-    print(out)
+def RenderTemplates(palette: list) -> str:
+    templates = Environment(loader=FileSystemLoader("templates"))
+    sway_template = templates.get_template("swaync.css.j2")
+    out = sway_template.render({"palette": palette})
+    return out
 
 
+def SaveTemplate(output, template: str):
+    with open(output, "w") as file:
+        file.write(template)
+
+
+def SymLink(src: PathLike, dst: PathLike):
+    os.symlink("", "", target_is_directory=True)
+
+
+output_location = "output"
 img = sys.argv[1]
-color_pallete = GetColors(img)
-print(color_pallete[0][0])
 
 out_path = Path(output_location)
 out_path.mkdir(exist_ok=True)
 
+color_pallete = GetColors(img)
+templates = RenderTemplates(color_pallete)
+SaveTemplate()
+
+SymLink(out_path, 123)
+
+
 img_path = Path(img)
 img_name = img_path.stem
-
-# Tester()
